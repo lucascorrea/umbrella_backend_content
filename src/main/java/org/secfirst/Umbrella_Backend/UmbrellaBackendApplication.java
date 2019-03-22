6f9605b31816a3ebfcf942d21a196a3be5a43d83
Douglas Mesquita <douglasalipio@gmail.com>
2019-03-22 22:05:56 +0000

    Update java 11

---
+++ src/main/java/org/secfirst/Umbrella_Backend/UmbrellaBackendApplication.java
@@ -0,0 +1,12 @@
+package org.secfirst.Umbrella_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellaBackendApplication.class, args);
+	}
+}
